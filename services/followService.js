419ba29c79d0a5187cbaf8aea1971f748b24f957
Shorlo <shorlo.sbg@gmail.com>
2023-04-08 00:07:39 +0200

    cleaning method

--- services/followService.js
+++ services/followService.js
@@ -5,24 +5,21 @@ const followUserIds = async(identityUserId) =>
     try
     {
         // Get info about following and followers
-
-        let following = await Follow.find({'user': identityUserId}).select({'followed': 1, '_id': 0}).exec();
-
-        let followers = await Follow.find({'followed': identityUserId}).select({'user': 1, '_id': 0}).exec();
-
-        // Get identities array
         let followingClean = [];
+        let followersClean = [];
+        let following = await Follow.find({'user': identityUserId}).select({'followed': 1, '_id': 0}).exec();
+        let followers = await Follow.find({'followed': identityUserId}).select({'user': 1, '_id': 0}).exec();
+        
+        // Get identities array cleaned
         following.forEach(follow =>
         {
             followingClean.push(follow.followed);
         });
-
-        let followersClean = [];
+     
         followers.forEach(follow =>
         {
             followersClean.push(follow.user);
         });
-
 
         return { following: followingClean, followers: followersClean };
     }
